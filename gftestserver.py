950cb9574007aea40a73c97ae5e7f7125e4b1422
Miguel Paraz <mparaz@mparaz.com>
2022-02-03 23:58:45 +1100

    Added server test cases for header as part for initial buffer, and other bad behaviour.

--- gftestserver.py
+++ gftestserver.py
@@ -41,7 +41,6 @@ if __name__ == "__main__":
 
         # Receive and show the header, don't do anything with it
         print(s.recv(8192))
-
         
         if option == 1:
             # Option 1.  # Do not send anything.
@@ -68,32 +67,60 @@ if __name__ == "__main__":
         elif option == 5:
             # Option 5. Send a non-decimal length
             s.send(b"GETFILE OK badlengthisbad\r\n\r\n")
+        elif option == 6:
+            # Option 6. Send a bad character in the middle of the CR LF
+            s.send(b"GETFILE OK 123\r\n!\r\n")
+        elif option == 7:
+            # Option 7. Send a file containing CR LF
+            s.send(b"GETFILE OK 2\r\n\r\n\r\n")
+        elif option == 8:
+            # Option 8. Send a file containing CR LF CR LF
+            s.send(b"GETFILE OK 4\r\n\r\n\r\n\r\n")
         elif option == 1000:
             # Serve a 2 GB + 1 (exceeds int) file
             # This can be verified with: sha1sum filename.
             hash = hashlib.sha1()
-            size = 2**31 + 1
+            orig_size = 2**31 + 1
+            size = orig_size
+            buffer_size = 8192
 
             s.send(bytes(f"GETFILE OK {size}\r\n\r\n", "UTF-8"))
             while size > 0:
-                buffer_size = 8192
-                random_buffer = bytes([random.randint(0, 255) for _ in range(0, buffer_size)])
+                random_buffer = random_bytes(buffer_size)
                 hash.update(random_buffer)
                 s.send(random_buffer)
                 size -= buffer_size
 
-            print(hash.hexdigest())            
-        else:
+            print('XXX warning, sha1sum does not match files XXX')
+            print(f"size={orig_size}, sha1sum={hash.hexdigest()}")
+        elif option == 1001:
             # Serve a random sized file with random-sized buffers of random bytes, and show the SHA1 hash.
             # This can be verified with: sha1sum filename.
+            #
+            # header is part of the buffer.
             hash = hashlib.sha1()
-            size = random.randint(10_000, 1_000_000)
+            orig_size = random.randint(10_000, 1_000_000)
+            size = orig_size
 
-            # numpy? from Vladimir.
-            # random_buffer = np.random.bytes(buffer_size)
+            header_buffer = bytes(f"GETFILE OK {size}\r\n\r\n", "UTF-8")
+            header_buffer_size = len(header_buffer)
 
+            payload_size = random.randint(min(100, size), min(1_000, size))
+            random_payload = random_bytes(payload_size)
+            
+            first_buffer = header_buffer + random_payload
+            first_buffer_size = len(first_buffer)
+            
+            print(f"header size {header_buffer_size} + payload size {payload_size} = first buffer size {first_buffer_size}")
 
-            s.send(bytes(f"GETFILE OK {size}\r\n\r\n", "UTF-8"))
+            # Hash only includes the payload, as the header is not stored on the client.
+            hash.update(random_payload)
+            s.send(first_buffer)
+
+            # Size to send only includes the payload, not the header.
+            size -= payload_size
+            print(f"remaining size {size}")
+
             while size > 0:
                 buffer_size = random.randint(min(100, size), min(1_000, size))
                 random_buffer = bytes([random.randint(0, 255) for _ in range(0, buffer_size)])
@@ -101,5 +128,22 @@ if __name__ == "__main__":
                 s.send(random_buffer)
                 size -= buffer_size
 
-            print(hash.hexdigest())
-            
+            print(f"size={orig_size}, sha1sum={hash.hexdigest()}")
+        else:
+            # Serve a random sized file with random-sized buffers of random bytes, and show the SHA1 hash.
+            # This can be verified with: sha1sum filename.
+            #
+            # header is not part of the buffer.
+            hash = hashlib.sha1()
+            orig_size = random.randint(10_000, 1_000_000)
+            size = orig_size
+
+            s.send(bytes(f"GETFILE OK {size}\r\n\r\n", "UTF-8"))
+            while size > 0:
+                buffer_size = random.randint(min(100, size), min(1_000, size))
+                random_buffer = random_bytes(buffer_size)
+                hash.update(random_buffer)
+                s.send(random_buffer)
+                size -= buffer_size
+
+            print(f"size={orig_size}, sha1sum={hash.hexdigest()}")
